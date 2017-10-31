552884d791c4d84762127fb7d81f1af01029f2b8
zhenyou.guo <zhenyou.guo@hand-china.com>
2017-10-31 09:30:15 +0800

    add gitignore

---
+++ fake-service/src/main/java/com/example/demo/SchedulingConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/***
+ * @author zhenyou.guo
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void scheduler() {
+        System.out.println("scheduled hello hello hello");
+    }
+}
