bd00bcdcc3f80fbd08878a39ab77d017a2d75e74
zhenyou.guo <zhenyou.guo@hand-china.com>
2017-11-01 19:13:21 +0800

    add gitignore

--- fake-service/src/main/java/com/example/demo/FakeController.java
+++ fake-service/src/main/java/com/example/demo/FakeController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,18 @@ public class FakeController {
     public String echo(@RequestBody String content) {
         return content + "from echo1 service";
     }
+
     @PostMapping("/echo/test2")
     public String echo2(@RequestBody String content) {
         return content + "from echo2 service";
+    }
+
+    @PostMapping(value = "/mixc/fake", produces = "application/json")
+    public String mixcFakeService(@RequestBody String body) {
+        JSONObject result = new JSONObject();
+        result.put("ErrCode", "ERR007");
+        result.put("ErrDesc", "Ops,Error!");
+        result.put("Result", JSON.parse(body));
+        return result.toJSONString();
     }
 }
