9f82122319bea69b80f33b4b2f219525a812270a
zhenyouguo <83946759@qq.com>
2017-11-05 19:09:25 +0800

    实现了根据请求body中的字段进行限流的功能

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,9 +1,21 @@
 package com.example.gateway;
 
+import com.alibaba.fastjson.JSON;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.DefaultRateLimitKeyGenerator;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.RateLimitKeyGenerator;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.properties.RateLimitProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +24,22 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
+	@Bean
+    public RateLimitKeyGenerator rateLimitKeyGenerator(final RateLimitProperties properties) {
+        return new DefaultRateLimitKeyGenerator(properties) {
+            @Override
+            public String key(HttpServletRequest request, Route route, RateLimitProperties.Policy policy) {
+                String subId = "";
+                try {
+                    InputStream in = request.getInputStream();
+                    String body = StreamUtils.copyToString(in, Charset.forName("UTF-8"));
+                    subId = JSON.parseObject(body).getString("subId");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return super.key(request, route, policy) + ":" + subId;
+            }
+        };
+    }
 }
